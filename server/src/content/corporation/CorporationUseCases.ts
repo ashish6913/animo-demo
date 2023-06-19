5f39314165310252f7fcb189d16f0e0c2ee1d45b
Ashish <gupta6913@gmail.com>
2023-06-19 11:50:54 -0400

    Remove invalid imports

--- server/src/content/corporation/CorporationUseCases.ts
+++ server/src/content/corporation/CorporationUseCases.ts
@@ -1,10 +1,7 @@
 import type { CharWithUseCases } from '../types'
 
 import { Corporation } from './Corporation'
-import { AirTravel } from './useCases/Airtravel'
-import { Conference } from './useCases/Conference'
 import { GovernmentGrant } from './useCases/GovernmentGrant'
-import { Hotel } from './useCases/Hotel'
 import { TaxCompliance } from './useCases/TaxCompliance'
 
 export const CorporationUseCases: CharWithUseCases = {
